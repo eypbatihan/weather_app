819d099ae616891e5d0359c1f496777fcb4a56f0
eypbatihan <eypbatihan@gmail.com>
2022-04-15 01:27:47 +0300

    setup

---
+++ weather-app/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+function App() {
+  return <div className="App">selam</div>;
+}
+
+export default App;
