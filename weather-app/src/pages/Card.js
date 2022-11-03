c9e2bb0b087f6b4554a27748fddcaa3167c18bcf
eypbatihan <eypbatihan@gmail.com>
2022-11-03 18:55:19 +0300

    card

--- weather-app/src/pages/Card.js
+++ weather-app/src/pages/Card.js
@@ -4,7 +4,9 @@ const Card = () => {
   return (
     <div className='card'>
         <header><h3>Ankara</h3>
-        <h1>35</h1>  
+        <h1>35</h1> 
+        <p>Sunny</p>
+        <p><span>H:79</span><span>L:56</span></p> 
         </header>
     </div>
   )
