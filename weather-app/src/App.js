42d3baa314fcdf3d5c3b1a603172076e2f44767a
eypbatihan <eypbatihan@gmail.com>
2022-11-03 17:23:11 +0300

    start

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Card from "./pages/Card";
 
 //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 function App() {
-  return <div className="App">selam</div>;
+  return <div className="App">
+    <div className="container">
+      <input placeholder="city name search"/>
+      <button type="submit">Search</button>
+      <Card/>
+    </div>
+  </div>;
 }
 
 export default App;
---
+++ weather-app/src/pages/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+        <header><h3>Ankara</h3>
+        <h1>35</h1>  
+        </header>
+    </div>
+  )
+}
+
+export default Card
